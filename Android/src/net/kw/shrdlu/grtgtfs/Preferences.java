c70b4a7d1d80d486116e06ba767ab384d2c97207
Giles Malet <gdmalet+github@gmail.com>
2011-09-19 22:31:32 -0400

    Added missing files to commit

---
+++ Android/src/net/kw/shrdlu/grtgtfs/Preferences.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2011 Giles Malet.
+ *
+ * This file is part of GRTransit.
+ * 
+ * GRTransit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GRTransit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GRTransit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.kw.shrdlu.grtgtfs;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+public class Preferences {
+
+	private Context mContext;
+	private String mPrefsFile;
+	private final String mPrefFavstops = "favstops";
+	
+	public Preferences(Context context) {
+		mContext = context;
+		mPrefsFile = mContext.getApplicationInfo().packageName;
+	}
+	
+	public void SaveBusstopFavourite(String busstop) {
+		SharedPreferences prefs = mContext.getSharedPreferences(mPrefsFile, Context.MODE_PRIVATE);
+		String favs = prefs.getString(mPrefFavstops, "");
+	
+		TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(';');
+		splitter.setString(favs);
+		boolean already = false;
+		for (String s : splitter) {
+			if (s.equals(busstop)) {
+				already = true;
+				break;
+			}
+		}
+	   
+		if (already) {
+			Toast.makeText(mContext, "Stop " + busstop + " is already a favourite!",
+					Toast.LENGTH_LONG).show();;
+		} else {
+			favs += busstop + ";";
+			prefs.edit().putString("favstops", favs).commit();
+			Toast.makeText(mContext, "Stop " + busstop + " was added to your favourites",
+					Toast.LENGTH_LONG).show();;
+		}
+	}
+	
+}
