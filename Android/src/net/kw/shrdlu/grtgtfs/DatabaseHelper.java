a5cd34da6657f1d6ca7b0fc636d001fee20a398e
Giles Malet <gdmalet+github@gmail.com>
2012-06-26 10:43:13 -0400

    New schedules, bumped version to 0.98.3, versioncode 15, db 9.

--- Android/src/net/kw/shrdlu/grtgtfs/DatabaseHelper.java
+++ Android/src/net/kw/shrdlu/grtgtfs/DatabaseHelper.java
@@ -41,7 +41,7 @@ public class DatabaseHelper {
 	private static final String TAG = "DatabaseHelper"; // getClass().getSimpleName();
 
 	private static String DB_PATH = null;
-	private static final int DB_VERSION = 8; /* As of 27th April 2012 */
+	private static final int DB_VERSION = 9; /* As of 25th June 2012 */
 	private static final String DB_NAME = "GRT.db";
 	private static Context mContext;
 	private static boolean mMustCopy = false;
